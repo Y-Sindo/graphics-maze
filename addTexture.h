33b93d30fa84253557664b89ef146d8386748549
MatildaX <605830409@qq.com>
2016-01-04 00:25:44 +0800

    Create addTexture.h

---
+++ addTexture.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <windows.h>
+#include <GL/glut.h>
+unsigned char *LoadBitmapFile(char *filename, BITMAPINFOHEADER *bitmapInfoHeader);
+void texload(int i,char *filename,GLuint *texture);
+void getTexture(GLuint* texture);
