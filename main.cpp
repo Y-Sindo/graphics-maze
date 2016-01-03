c7114faf1de2b2bc30dd80cf9cf3d5779af5c100
Y-Sindo <Y-Sindo@users.noreply.github.com>
2016-01-03 14:22:46 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,15 +11,19 @@ using namespace std;
 #define BLOCK_SIZE (10.0)
 #define BLOCK_HEIGHT (20.0)
 #define PI (3.1415926)
+#define MAZE_SIZE 21
 
-int**  matrix;
+vector<vector<int>> maze_matrix;
+
 GLdouble eye_pos[3] = { 1.5*BLOCK_SIZE, 8, 0, };
 GLdouble center[3] = { 1.5*BLOCK_SIZE, 8, -2 * BLOCK_SIZE };//c=e+ 旋转后的d
 GLdouble angle = -PI / 2;  //方向向量与x轴的夹角
 						  //长度为2*BLOCK_SIZE	
 GLfloat view_width, view_height;
 
-void Draw_Maze( int **  matrix,int matrix_size) 
+bool view_first=true;
+
+void Draw_Maze(vector<vector<int>>&  matrix, int matrix_size)
 {
 	GLint wall_color[4] = { 1, 1, 1, 1 };
 	glMaterialiv(GL_FRONT_AND_BACK, GL_AMBIENT, wall_color);
@@ -39,20 +43,11 @@ void Draw_Maze( int **  matrix,int matrix_size)
 
 void redraw()
 {
-	//生成迷宫
-	vector<vector<int>> maze;
-	getNewMaze(10, 10, maze);
-	// maze有一个入口和一个出口，maze[1][0],maze[2*h-1][2*w]
-	/*for (int i = 0; i < 10 * 2 + 1; i++) {
-		for (int j = 0; j < 10 * 2 + 1; j++) {
-			cout << maze[i][j];
-		}
-		cout << endl;
-	}*/
+	
 
 	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);//开启深度测试之前必须调用这个函数。
 	glPushMatrix();								
-	Draw_Maze(matrix, 6);
+	Draw_Maze(maze_matrix, 21);
 	glPopMatrix();
 
 	glutSwapBuffers();
@@ -71,12 +66,19 @@ void updateView(int width, int height){
 	center[0] = length * cosf(angle) + eye_pos[0];
 	center[2] = length * sinf(angle) + eye_pos[2];
 	glLoadIdentity();
-	gluLookAt(eye_pos[0], eye_pos[1], eye_pos[2], center[0], center[1], center[2], 0, 1, 0);
+
+	if (view_first)
+		gluLookAt(eye_pos[0], eye_pos[1], eye_pos[2], center[0], center[1], center[2], 0, 1, 0);//第一人称视角
+	else
+		gluLookAt(0, 60, 0, 0, 0, 0, 0, 0, -1);  //俯视视角
+	
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(120.f, (GLfloat)width / (GLfloat)height, 4.0f, 40.0f);
-	//glOrtho(0,100,0,100,0,40);//???????   俯视
+	if (view_first)
+		gluPerspective(120.f, (GLfloat)width / (GLfloat)height, 4.0f, 40.0f);//第一人称
+	else
+		glOrtho(0,210,0,210,0,60);//  俯视
 	glMatrixMode(GL_MODELVIEW);
 
 
@@ -91,7 +93,7 @@ void reshape(int width, int height)
 	
 	updateView(width,height);
 
-	//gluLookAt(0, 60, 0, 0, 0, 0, 0, 0, -1);  //俯视视角
+	
 
 
 
@@ -115,7 +117,8 @@ bool collision_detection(GLdouble new_x, GLdouble new_z){
 			tmp_z = new_z + 2 * q;
 			int i = tmp_x / BLOCK_SIZE;
 			int j = -tmp_z / BLOCK_SIZE;
-			if (matrix[i][j] == 1) return true;//代表碰撞
+			if (i < 0 || j < 0 || i >= MAZE_SIZE || j >= MAZE_SIZE) return true;//超出地图范围
+			if (maze_matrix[i][j] == 1) return true;//代表碰撞
 		}
 	return false;
 
@@ -142,17 +145,26 @@ void key(unsigned char k, int x, int y){
 			eye_pos[2] += sin(angle)*length;
 		}
 		break;
-	case 'd'://右转，角度增加10度
+
+	case 'd':
 		angle += 10.0 / 180 * PI;
-		break;
-	case 'a'://左转，角度减小10度
+		break;//右转，角度增加10度
+
+	case 'a':
 		angle -= 10.0 / 180 * PI;
+		break;//左转，角度减小10度
+
+	case 'v':
+		view_first = !view_first;
 		break;
 
-	case '0':
+	case '0'://切换第一人称视图和俯视视图
 		newScreenShot(700, 700, "ScreenShots/tmp.bmp");
+		break;
+
 	}
 	updateView(view_width,view_height);
+
 }
 
 
@@ -170,13 +182,18 @@ int main(int argc, char *argv[])
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
 	glutInitWindowSize(700, 700);      
-	matrix= new int*[6];
-	matrix[0] = new int[6]{1, 1, 1, 1, 1, 1};
-	matrix[1] = new int[6]{0, 0, 1, 0, 0, 1};
-	matrix[2] = new int[6]{1, 0, 0, 1, 0, 1};
-	matrix[3] = new int[6]{1, 1, 0, 0, 1, 1};
-	matrix[4] = new int[6]{1, 1, 1, 0, 0, 1};
-	matrix[5] = new int[6]{1, 1, 1, 1, 1, 1};
+
+
+
+	getNewMaze(10, 10, maze_matrix);
+	//生成迷宫
+	// maze有一个入口和一个出口，maze[1][0],maze[2*h-1][2*w]
+	for (int i = 0; i < 10 * 2 + 1; i++) {
+	for (int j = 0; j < 10 * 2 + 1; j++) {
+	cout << maze_matrix[i][j];
+	}
+	cout << endl;
+	}
 	int windowHandle
 		= glutCreateWindow("Maze-Game");
 
@@ -186,9 +203,6 @@ int main(int argc, char *argv[])
 	glutKeyboardFunc(key);
 	glutMainLoop();
 
-	for (int i = 0; i < 6; i++)
-		delete[]matrix[i];
-	delete[]matrix;
 	return 0;
 }
 
